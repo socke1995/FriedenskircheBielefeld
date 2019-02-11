4ad6cbdb88f1b63ec2d61bac5b19cf7b1861291f
Daniel Willems <dwillems@gmx.net>
2019-02-11 23:56:00 +0100

    Adding Passwort check and Segue to new View with multiple bottom controls

---
+++ FriedenskircheBielefeld/VC_Main.swift
@@ -0,0 +1,39 @@
+//
+//  VC_Main.swift
+//  FriedenskircheBielefeld
+//
+//  Created by Daniel Willems on 11.02.19.
+//  Copyright © 2019 Daniel Willems. All rights reserved.
+//
+
+import UIKit
+
+class VC_Main: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func ftp(){
+        let _host = "daniewillems.ddns.net"
+        let _user = "willems"
+        let _password = "FSMv0RXfUYfgQ07"
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FriedenskircheBielefeld/ViewController.swift
+++ FriedenskircheBielefeld/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var errorMessage: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
+    @IBAction func login(_ sender: UIButton) {
+        if passwordTextField.text == "predigten"{
+            print("Das hat geklappt");
+            performSegue(withIdentifier: "segueToMain", sender: self);
+        }else{
+            errorMessage.text = "Das Passwort ist leider falsch"
+        }
+    }
+    
 }
 
